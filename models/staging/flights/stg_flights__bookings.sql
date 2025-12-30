2a6af555c5270e0f92912a1996e940d30806c483
Oleh Cherednichenko <olegche@amazon.com>
2025-12-30 15:05:09 +0200

    new models created

--- models/staging/flights/stg_flights__bookings.sql
+++ models/staging/flights/stg_flights__bookings.sql
@@ -1,6 +1,14 @@
 {{
     config(
-        materialized='table'
+        materialized='table',
+        on_configuration_change='apply',
+        indexes=[
+            {
+                'columns': ['book_date'],
+                'unique': false
+            },
+
+        ]
     )
 }}
 SELECT
